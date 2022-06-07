d2c01ab62cd1faf11fb2058ee9615762b01eccd5
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-06-08 01:55:07 +0700

    delete: menghapus judul dan input pencarian di halaman anime

--- pages/anime/index.js
+++ pages/anime/index.js
@@ -15,7 +15,7 @@ const Anime = () => {
   const [title, setTitle] = useState("Top Upcoming Anime");
 
   useEffect(() => {
-    const getAnime = async (type) => {
+    const getAnime = async (path) => {
       const request = await fetch(`${JIKAN_API}/seasons/${path}`);
       const response = await request.json();
       setAnime(response.data);
@@ -29,35 +29,6 @@ const Anime = () => {
 
   return (
     <section className="min-h-screen bg-slate-800 pt-5 pb-10 text-white">
-      <div className="container mb-8">
-        <div className="flex flex-col md:flex-row-reverse md:items-center md:justify-between">
-          <div className="w-1/2 md:w-1/3">
-            <form>
-              <div className="relative w-full">
-                <select
-                  className="block appearance-none w-full bg-slate-900 border border-slate-800 text-white py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-slate-700 focus:border-slate-700"
-                  onChange={handleClick}
-                >
-                  <option value="now">Now Airing</option>
-                  <option value="upcoming">Upcoming</option>
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-slate-400">
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div>
-            <TitleSection className="my-4">{title}</TitleSection>
-          </div>
-        </div>
-      </div>
       <div className="container px-0 md:px-4 min-h-screen">
         {!isLoading ? (
           <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 md:gap-6">
