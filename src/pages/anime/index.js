fb0405fb935d2eaf1193ea366c6b46148497a14a
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-03 00:01:58 +0700

    update: perbaikan kecil

--- src/pages/anime/index.js
+++ src/pages/anime/index.js
@@ -34,11 +34,13 @@ const Anime = () => {
 
   const getData = async (selectedAnime, page) => {
     const res = await getAnimeWithPagination(selectedAnime, page);
-    if (res) {
-      setJikanAnime(res.data);
-      setPaginate(res.pagination);
-    } else setIsError(true);
-    setIsLoading(false);
+    if (res !== undefined) {
+      setJikanAnime(await res.data);
+      setPaginate(await res.pagination);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
   };
 
   const dropdownHandler = (e) => {
@@ -53,52 +55,33 @@ const Anime = () => {
 
   return (
     <section className="min-w-full bg-gradient-to-tl from-slate-900 via-slate-800 to-slate-900 py-4 min-h-screen">
-      <RenderIfTrue isTrue={isError}>
-        <div className="container">
-          <ErrorMessage message="Ada sedikit kesalahan pada API nya, coba refresh kembali." />
-        </div>
-      </RenderIfTrue>
-      <RenderIfFalse isFalse={isError}>
-        <div className="container flex flex-col sm:flex-row justify-between items-start sm:items-center gap-6 sm:gap-0 pt-4 pb-7">
-          <TitleSection>{title}</TitleSection>
-          <Dropdown
-            dataDropdown={dataDropdownAnime}
-            onClick={dropdownHandler}
-          />
-        </div>
-        <div className="container px-0 lg:px-4">
-          <RenderIfTrue isTrue={isLoading}>
-            <Loading />
-          </RenderIfTrue>
-          <RenderIfFalse isFalse={isLoading}>
-            <RenderIfTrue isTrue={jikanAnime.length > 0}>
-              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-6">
-                <For
-                  each={jikanAnime}
-                  render={({ mal_id, images, title, score }) => (
-                    <MainCard
-                      key={mal_id}
-                      path={`/anime/${mal_id}/details`}
-                      id={mal_id}
-                      image={images?.webp?.large_image_url}
-                      title={title}
-                      score={score}
-                      py="py-5"
-                      fontsize="text-base"
-                    />
-                  )}
-                />
-              </div>
-            </RenderIfTrue>
-            <RenderIfFalse isFalse={jikanAnime.length > 0}>
-              <div className="container">
-                <ErrorMessage message="Gagal mengambil data dari API, coba refresh ulang browsernya" />
-              </div>
-            </RenderIfFalse>
-          </RenderIfFalse>
-        </div>
-        <RenderIfFalse isFalse={isError}>
-          <RenderIfFalse isFalse={isLoading}>
+      <div className="container flex flex-col sm:flex-row justify-between items-start sm:items-center gap-6 sm:gap-0 pt-4 pb-7">
+        <TitleSection>{title}</TitleSection>
+        <Dropdown dataDropdown={dataDropdownAnime} onClick={dropdownHandler} />
+      </div>
+      <div className="container px-0 lg:px-4">
+        <RenderIfTrue isTrue={isLoading}>
+          <Loading />
+        </RenderIfTrue>
+        <RenderIfFalse isFalse={isLoading}>
+          <RenderIfTrue isTrue={jikanAnime.length > 0}>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-6">
+              <For
+                each={jikanAnime}
+                render={({ mal_id, images, title, score }) => (
+                  <MainCard
+                    key={mal_id}
+                    path={`/anime/${mal_id}/details`}
+                    id={mal_id}
+                    image={images?.webp?.large_image_url}
+                    title={title}
+                    score={score}
+                    py="py-5"
+                    fontsize="text-base"
+                  />
+                )}
+              />
+            </div>
             <div className="md:hidden container my-6 flex justify-center items-center">
               <div>
                 <p className="text-lg md:text-xl text-center">
@@ -122,9 +105,14 @@ const Anime = () => {
                 </Button>
               </RenderIfTrue>
             </div>
+          </RenderIfTrue>
+          <RenderIfFalse isFalse={jikanAnime.length > 0}>
+            <div className="container">
+              <ErrorMessage message="Gagal mengambil data dari API, coba refresh ulang browsernya" />
+            </div>
           </RenderIfFalse>
         </RenderIfFalse>
-      </RenderIfFalse>
+      </div>
     </section>
   );
 };
--- src/pages/anime/page/[page].js
+++ src/pages/anime/page/[page].js
@@ -26,7 +26,6 @@ const AnimePagination = () => {
   const [paginate, setPaginate] = useState({});
   const [jikanAnime, setJikanAnime] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
 
   const previousPageHandler = () => {
     setIsLoading(true);
@@ -41,15 +40,12 @@ const AnimePagination = () => {
 
   const getData = async (selectedAnime, page) => {
     const res = await getAnimeWithPagination(selectedAnime, page);
-    if (res) {
+    if (res !== undefined) {
       setJikanAnime(await res.data);
       setPaginate(await res.pagination);
       setTimeout(() => {
         setIsLoading(false);
-      }, 900);
-    } else {
-      setIsError(true);
-      setIsLoading(false);
+      }, 1000);
     }
   };
 
@@ -65,52 +61,33 @@ const AnimePagination = () => {
 
   return (
     <section className="min-w-full bg-gradient-to-tl from-slate-900 via-slate-800 to-slate-900 py-4 min-h-screen">
-      <RenderIfTrue isTrue={isError}>
-        <div className="container">
-          <ErrorMessage message="Ada sedikit kesalahan pada API nya, coba refresh kembali." />
-        </div>
-      </RenderIfTrue>
-      <RenderIfFalse isFalse={isError}>
-        <div className="container flex flex-col sm:flex-row justify-between items-start sm:items-center gap-6 sm:gap-0 pt-4 pb-7">
-          <TitleSection>{title}</TitleSection>
-          <Dropdown
-            dataDropdown={dataDropdownAnime}
-            onClick={dropdownHandler}
-          />
-        </div>
-        <div className="container px-0 lg:px-4">
-          <RenderIfTrue isTrue={isLoading}>
-            <Loading />
-          </RenderIfTrue>
-          <RenderIfFalse isFalse={isLoading}>
-            <RenderIfTrue isTrue={jikanAnime.length > 0}>
-              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-6">
-                <For
-                  each={jikanAnime}
-                  render={({ mal_id, images, title, score }) => (
-                    <MainCard
-                      key={mal_id}
-                      path={`/anime/${mal_id}/details`}
-                      id={mal_id}
-                      image={images?.webp?.large_image_url}
-                      title={title}
-                      score={score}
-                      py="py-5"
-                      fontsize="text-base"
-                    />
-                  )}
-                />
-              </div>
-            </RenderIfTrue>
-            <RenderIfFalse isFalse={jikanAnime.length > 0}>
-              <div className="container">
-                <ErrorMessage message="Gagal mengambil data dari API, coba refresh ulang browsernya" />
-              </div>
-            </RenderIfFalse>
-          </RenderIfFalse>
-        </div>
-        <RenderIfFalse isFalse={isError && isLoading}>
-          <RenderIfFalse isFalse={isLoading}>
+      <div className="container flex flex-col sm:flex-row justify-between items-start sm:items-center gap-6 sm:gap-0 pt-4 pb-7">
+        <TitleSection>{title}</TitleSection>
+        <Dropdown dataDropdown={dataDropdownAnime} onClick={dropdownHandler} />
+      </div>
+      <div className="container px-0 lg:px-4">
+        <RenderIfTrue isTrue={isLoading}>
+          <Loading />
+        </RenderIfTrue>
+        <RenderIfFalse isFalse={isLoading}>
+          <RenderIfTrue isTrue={jikanAnime.length > 0}>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-6">
+              <For
+                each={jikanAnime}
+                render={({ mal_id, images, title, score }) => (
+                  <MainCard
+                    key={mal_id}
+                    path={`/anime/${mal_id}/details`}
+                    id={mal_id}
+                    image={images?.webp?.large_image_url}
+                    title={title}
+                    score={score}
+                    py="py-5"
+                    fontsize="text-base"
+                  />
+                )}
+              />
+            </div>
             <div className="md:hidden container my-6 flex justify-center items-center">
               <div>
                 <p className="text-lg md:text-xl text-center">
@@ -141,9 +118,14 @@ const AnimePagination = () => {
                 </Button>
               </RenderIfTrue>
             </div>
+          </RenderIfTrue>
+          <RenderIfFalse isFalse={jikanAnime.length > 0}>
+            <div className="container">
+              <ErrorMessage message="Gagal mengambil data dari API, coba refresh ulang browsernya" />
+            </div>
           </RenderIfFalse>
         </RenderIfFalse>
-      </RenderIfFalse>
+      </div>
     </section>
   );
 };
